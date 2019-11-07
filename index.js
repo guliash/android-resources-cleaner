cbfb9b7023a507471cd7f9a693385566630a06b2
Artem Gilmudinov <guliash92@gmail.com>
2019-11-07 21:06:21 +0300

    handle more resources types;

--- index.js
+++ index.js
@@ -1,35 +1,118 @@
 const fs = require('fs');
 
+const MODE = {
+  DRAWABLE: 'drawable',
+  COLOR: 'color',
+  DIMEN: 'dimen',
+  LAYOUT: 'layout'
+};
+
 const ALLOWED_EXTENSIONS = ['java', 'kt', 'xml'];
 
-const rootDir = process.argv[2];
-const resourcesDir = process.argv[3];
+const mode = process.argv[2];
+const rootDir = process.argv[3];
+const resourcesPath = process.argv[4];
 
-const findUnusedResources = async (rootDir, resourcesDir) => {
+const findUnusedResources = async (rootDir, resourcesPath, mode) => {
   const modules = await findAllModules(rootDir);
   
   const modulesSources = await Promise.all(modules.map(it => findAllSourceFiles(it + '/src')));
 
   const sources = modulesSources.reduce((prev, cur) => prev.concat(cur), []);
 
-  const resources = await readdir(resourcesDir);
-
-  const resourcesNames = resources.map(it => it.split('.')[0]);
+  const resources = await retrieveResources(resourcesPath, mode);
   
-  const usedResourcesPerFile = await Promise.all(sources.map(it => findResourcesUsedInFile(it, resourcesNames)));
+  const usedResourcesPerFile = await Promise.all(sources.map(it => findResourcesUsedInFile(it, resources, mode)));
 
   const usedResources = usedResourcesPerFile.reduce((prev, cur) => prev.concat(cur), []);
 
-  const unusedResources = resourcesNames.filter(name => usedResources.find(it => it === name) == null);
+  const unusedResources = resources.filter(name => usedResources.find(it => it === name) == null);
 
-  await Promise.all(unusedResources.map(it => unlink(resourcesDir + '/' + it + '.xml')));
+  if (mode === MODE.DRAWABLE || mode === MODE.LAYOUT) {
+    await Promise.all(unusedResources.map(it => unlink(resourcesPath + '/' + it + '.xml')));
+  }
   return unusedResources;
 }
 
-const findResourcesUsedInFile = async (file, resources) => {
+const retrieveResources = async (resourcesPath, mode) => {
+  const retrieveFileResources = async () => {
+    const resources = await readdir(resourcesPath);
+    return resources.map(it => it.split('.')[0]);
+  };
+
+  const retrieveResourcesFromFile = async () => {
+    const data = await readFile(resourcesPath);
+    const tokens = data.split('\n');
+    const tag = resourceXmlTag(mode);
+    const regex = new RegExp(`${ tag } name="(.*)"`);
+    return tokens
+      .map(token => {
+        const match = token.match(regex);
+        if (match != null) {
+          return match[1];
+        }
+        return null;
+      })
+      .filter(it => it != null);
+  };
+
+  if (mode === MODE.DRAWABLE || mode === MODE.LAYOUT) {
+    const result = await retrieveFileResources();
+    return result;
+  }
+  if (mode === MODE.COLOR || mode === MODE.DIMEN) {
+    const result = await retrieveResourcesFromFile();
+    return result;
+  }
+}
+
+const findResourcesUsedInFile = async (file, resources, mode) => {
   const data = await readFile(file);
 
-  return resources.filter(it => data.includes(`R.drawable.${ it }`) || data.includes(`drawable/${ it }`));
+  const codePrefix = codePrefixFromMode(mode);
+  const xmlPrefix = xmlPrefixFromMode(mode);
+  return resources.filter(it => data.includes(`${ codePrefix }${ it }`) || data.includes(`${ xmlPrefix }${ it }`));
+};
+
+const resourceXmlTag = mode => {
+  switch (mode) {
+    case MODE.COLOR:
+      return 'color';
+    case MODE.DIMEN:
+      return 'dimen';
+    default:
+      throw new Error(`Unknown mode ${ mode }`);
+  }
+};
+
+const codePrefixFromMode = mode => {
+  switch (mode) {
+    case MODE.DRAWABLE:
+      return 'R.drawable.';
+    case MODE.COLOR:
+      return 'R.color.';
+    case MODE.DIMEN:
+      return 'R.dimen.';
+    case MODE.LAYOUT:
+      return 'R.layout.';
+    default:
+      throw new Error(`Unknown mode ${ mode }`);
+  }
+};
+
+const xmlPrefixFromMode = mode => {
+  switch (mode) {
+    case MODE.DRAWABLE:
+      return 'drawable/';
+    case MODE.COLOR:
+      return 'color/';
+    case MODE.DIMEN:
+      return 'dimen/';
+    case MODE.LAYOUT:
+      return 'layout/';
+    default:
+      throw new Error(`Unknown mode ${ mode }`);
+  }
 };
 
 const findAllModules = async rootDir => {
@@ -89,7 +172,7 @@ const stat = async file => {
 
 const readFile = async file => {
   return new Promise((resolve, reject) => {
-    fs.readFile(file, (error, result) => {
+    fs.readFile(file, 'utf8', (error, result) => {
       if (error == null) {
         return resolve(result);
       }
@@ -109,10 +192,11 @@ const unlink = async file => {
   });
 };
 
-findUnusedResources(rootDir, resourcesDir)
+findUnusedResources(rootDir, resourcesPath, mode)
   .then(it => {
     console.log('Success');
     console.log(`Deleted ${ it.length } resources`);
+    console.log(it);
     process.exit(0);
   })
   .catch(error => {
